5c14288508ed4eab0ae426cfcb4112a08de01e68
Samrakshini <Samrakshini.S@target.com>
2020-12-17 08:22:38 +0530

    EDA, logistic regression

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A beginner project to predict if a person dies of heart failure',
+    author='Samrakshini',
+    license='',
+)
